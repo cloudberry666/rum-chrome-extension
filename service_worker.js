8ff5ef6dff2f2c0ab6b9f1eb2c8e3bc12e876c6c
wangzz2019 <jack.wang@datadoghq.com>
2021-11-09 21:31:53 +0900

    delete comment in service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 963c8006fd94eab27d2df0f098697dc8c2868c2e
 // var match_reg='[a-zA-z]+://[a-zA-Z]+\.wikipedia\.org/[^\s]*'
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
